8d491dd6f1ceafdeeedae1aade141fa280ec1c42
Wincy LAW <wincylaw@Wincys-MacBook-Pro.local>
2019-12-25 16:24:06 -0800

    calculates number of days to 1st offer

--- gaoutcomes_front/src/components/Students.js
+++ gaoutcomes_front/src/components/Students.js
@@ -18,6 +18,7 @@ class Students extends React.Component{
             <tr>
               <th>Name</th>
               <th>Date 1st Offer</th>
+              <th>Number of Days</th>
             </tr>
           </thead>
           <tbody>
@@ -26,7 +27,13 @@ class Students extends React.Component{
               <tr className="student-row" key={student.id}>
                 <td>{student.name}</td>
 
-                {student.dateoffer===null? <td>pending</td>: <td>{new Date(student.dateoffer).toLocaleDateString("en-US")}</td>}
+                {student.dateoffer===null? <td>pending</td>
+                : <td>{new Date(student.dateoffer).toLocaleDateString("en-US")}</td>}
+
+                {student.dateoffer===null?
+                <td>{Math.floor((new Date()-new Date(this.props.students[0].dategraduated))/86400000)}</td>
+                : <td>{Math.floor((new Date(student.dateoffer)-new Date(this.props.students[0].dategraduated))/86400000)}</td>}
+
 
                 <td className="table-button"><button onClick={() => {this.props.handleView("editStudent", student)}}>&#9998;</button></td>
                 <td className="table-button"><button onClick={() => {this.props.handleDelete(student.id)}}>&#128465;</button></td>
